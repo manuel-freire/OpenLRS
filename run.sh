8f40791400013b994bb8f170f27f1d9b59d367ee
Manuel Freire <manuel.freire@gmail.com>
2015-09-29 17:12:58 +0200

    Added dockerfile, updated run.sh to use docker-compose env. variables

--- run.sh
+++ run.sh
@@ -1,1 +1,18 @@
-mvn clean package spring-boot:run -Drun.jvmArguments="-Dspring.config.location=./src/main/resources/application-dev.properties"
+#!/bin/bash
+
+# see docker-compose run <container> env to see all
+
+R_PROPFILE=./src/main/resources/application-redis.properties
+R_HOST=${REDIS_HOST/*\//}
+R_HOST=${R_HOST/:*/}
+
+E_PROPFILE=./src/main/resources/application-elasticsearch.properties
+E_HOST=${ELASTIC_HOST/*\//}
+
+echo "Redis should be at: $R_HOST"
+echo "spring.redis.host=${R_HOST}" >> ${R_PROPFILE}
+
+echo "ElasticSearch should be at: $E_HOST"
+echo "spring.data.elasticsearch.cluster-nodes=${E_HOST}" > ${E_PROPFILE}
+
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.config.location=./src/main/resources/application-dev.properties"
